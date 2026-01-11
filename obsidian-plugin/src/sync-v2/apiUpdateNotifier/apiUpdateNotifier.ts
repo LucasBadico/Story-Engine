9e21b5abb0a49cdcfdade4834e976d5dd53f1cd7
Lucas Badico <lucasgomesdesantana@gmail.com>
2026-01-11 10:57:50 -0300

    obsidian-plugin: sync v2 api update notifier

---
+++ obsidian-plugin/src/sync-v2/apiUpdateNotifier/apiUpdateNotifier.ts
@@ -0,0 +1,8 @@
+import { ApiUpdateNotifierV2 } from "./ApiUpdateNotifierV2";
+
+/**
+ * Global instance of ApiUpdateNotifierV2
+ * This is used to notify about API updates from anywhere in the codebase
+ */
+export const apiUpdateNotifierV2 = new ApiUpdateNotifierV2();
+
